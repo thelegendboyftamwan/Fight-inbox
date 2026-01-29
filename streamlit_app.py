bae451be1a6307fd451cd3f1d9523d48124f8e14
thelegendboyftamwan <thelegendboyftamwan@gmail.com>
2026-01-29 21:38:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ import database as db
 import requests
 
 st.set_page_config(
-    page_title="YKTI RAWAT",
+    page_title="MR ZAIID X AMWAN",
     page_icon="✅",
     layout="wide",
     initial_sidebar_state="expanded"
